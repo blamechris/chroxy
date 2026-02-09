1b2d30a46d6e1891f9f0979dc34ffeca4760d6c3
BlameChris <ChrisPishaki@gmail.com>
2026-02-08 16:06:50 -0800

    fix(app): remove unused View import from TerminalView

--- packages/app/src/components/TerminalView.tsx
+++ packages/app/src/components/TerminalView.tsx
@@ -1,5 +1,5 @@
 import React, { useMemo } from 'react';
-import { View, Text, ScrollView, StyleSheet, Platform } from 'react-native';
+import { Text, ScrollView, StyleSheet, Platform } from 'react-native';
 
 // -- Props --
 
